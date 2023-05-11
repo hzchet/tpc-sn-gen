883fe35f12f051b26fb8f6b100640864ece29854
Amrin Aidar <aidaramrin@gmail.com>
2023-05-11 20:12:00 +0600

    Fix feature processing

--- data/loader.py
+++ data/loader.py
@@ -17,13 +17,12 @@ class MyDataset(Dataset):
 
 
 def get_loaders(scaler, batch_size=32, data_version=None, pad_range=(40, 50), time_range=(265, 280), strict=False):
-    data, features = read_csv_2d(pad_range=pad_range, time_range=time_range, strict=strict)
+    data, raw_features = read_csv_2d(pad_range=pad_range, time_range=time_range, strict=strict)
     data_scaled = scaler.scale(data).astype('float32')
-    features = preprocess_features(features.astype('float32'))
     
-    Y_train, Y_test, X_train, X_test = train_test_split(data_scaled, features, test_size=0.25, random_state=42)
+    Y_train, Y_test, X_train_raw, X_test_raw = train_test_split(data_scaled, raw_features, test_size=0.25, random_state=42)
     
-    train_dataset = MyDataset(Y_train, X_train)
+    train_dataset = MyDataset(Y_train, preprocess_features(X_train_raw.astype('float32')))
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=4, pin_memory=True)
-    return train_loader, X_test, Y_test
+    return train_loader, X_test_raw, Y_test
